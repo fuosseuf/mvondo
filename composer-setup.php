407e535b05e94ad856f182a905a17a096088a246
rimanno <rimanno7@yahoo.fr>
2016-04-09 18:27:55 +0100

    Modification du logo - Custom Header & Footer du site

---
+++ composer-setup.php
@@ -0,0 +1,4 @@
+
+Warning: readfile(): Unable to find the wrapper "https" - did you forget to enable it when you configured PHP? in Command line code on line 1
+
+Warning: readfile(https://getcomposer.org/installer): failed to open stream: Invalid argument in Command line code on line 1
